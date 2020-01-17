bd6a3af155f8dfffab760161c5084b8a766d396f
macedo <felipe.x.macedo@gmail.com>
2020-01-17 00:46:44 -0300

    Estudos da Aula Web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routs = require('./routes')
 
 const app = express();
@@ -9,7 +10,7 @@ mongoose.connect('mongodb+srv://mongodb-stitch-test-urxlj:7kYIpVlgqGLvzzkK@clust
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
+app.use(cors())
 app.use(express.json());
 app.use(routs);
 
